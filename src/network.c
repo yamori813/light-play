1f3a885c83139db0622a7edece7ed9b34ce58a3b
Hiroki Mori <yamori813@yahoo.co.jp>
2025-07-02 17:25:54 +0900

    FreeBSD support

--- src/network.c
+++ src/network.c
@@ -22,6 +22,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#ifdef __FreeBSD__
+#include <netinet/in.h>
+#endif
 #include <netdb.h>
 #include <stdlib.h>
 #include <unistd.h>
